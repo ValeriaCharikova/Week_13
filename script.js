9d0d9d89270d281107ff4e2b7a29bd43b04e3c10
Vlb92 <valeriiacharikova@gmail.com.com>
2022-10-09 21:15:14 +0300

    added files

---
+++ script.js
@@ -0,0 +1,28 @@
+function showMessage() {
+  let name = prompt("Как тебя зовут?");
+  alert(`Привет, ${name}!`);
+}
+
+function addition() {
+  let a = prompt("Первое число");
+  let b = prompt("Второе число");
+  alert(+a + +b);
+}
+
+function subtraction() {
+  let a = prompt("Первое число");
+  let b = prompt("Второе число");
+  alert(a - b);
+}
+
+function multiplication() {
+  let a = prompt("Первое число");
+  let b = prompt("Второе число");
+  alert(a * b);
+}
+
+function division() {
+  let a = prompt("Первое число");
+  let b = prompt("Второе число");
+  alert(a / b);
+}
